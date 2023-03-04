17ede3e83543ac54dc3a8ad31b2bf34ac105427b
alexa <alexa@ALEX>
2023-03-04 09:58:01 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/PlatosController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L01_2020_VH_602.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatosController : ControllerBase
+    {
+    }
+}
---
+++ Models/Rest.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace L01_2020_VH_602.Models
+{
+    public class Rest: DbContext
+    {
+        public Rest(DbContextOptions<Rest>options): base(options)
+        {
+
+        }
+    }
+}
